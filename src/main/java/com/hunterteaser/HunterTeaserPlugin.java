9ae01484c0922684f8941dc0f6d0ad6a91ac30c4
awesomeusernam3 <greglenhart@gmail.com>
2026-06-29 12:06:35 -0400

    Put in correct menu listener and removed example code

--- src/main/java/com/hunterteaser/HunterTeaserPlugin.java
+++ src/main/java/com/hunterteaser/HunterTeaserPlugin.java
@@ -14,8 +14,6 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
-
-
 @Slf4j
 @PluginDescriptor(
 	name = "Hunter Teaser"
@@ -25,35 +23,18 @@ public class HunterTeaserPlugin extends Plugin
 	@Inject
 	private Client client;
 
-	@Inject
-	private HunterTeaserConfig config;
-
 	private static final String[] HUNTER_TEASING_INSULTS = {
 			"Hey smelly!",
+			"Hey stinky!",
 			"You suck!",
 			"Loser says what?",
 			"You mad bro?"
-
 	};
-
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.debug("Example started!");
-	}
-
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.debug("Example stopped!");
-	}
 
 	@Subscribe
 	public void onMenuOptionClicked(MenuOptionClicked e)
 	{
-		if (e.getMenuOption().equals("Chip")
-			//	&& e.getMenuTarget().endsWith("Door")
-			)
+		if(e.getMenuOption().equals("Tease"))
 		{
 			Random rand = new Random();
 			int randomIndex = rand.nextInt(HUNTER_TEASING_INSULTS.length);
@@ -61,26 +42,6 @@ public class HunterTeaserPlugin extends Plugin
 
 			client.getLocalPlayer().setOverheadCycle(100);
 			client.getLocalPlayer().setOverheadText(insult);
-			//doorLocation = LocalPoint.fromScene(e.getParam0(), e.getParam1());
 		}
-//		else
-//		{
-//			//doorLocation = null;
-//		}
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
-
-	@Provides
-	HunterTeaserConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(HunterTeaserConfig.class);
 	}
 }
